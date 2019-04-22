6b38629b5cf054a3286114751d5e299423919e32
sasha <sashazemzer@gmail.com>
2019-04-22 23:57:27 +0300

    Initial commit

---
+++ src/main/java/org/donntu/knt/mksit/lab3/Main.java
@@ -0,0 +1,13 @@
+package org.donntu.knt.mksit.lab3;
+
+import java.io.IOException;
+
+import static org.donntu.knt.mksit.lab3.LZW.compress;
+import static org.donntu.knt.mksit.lab3.LZW.decompress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        compress("files/input.txt", "files/compressed_output.txt");
+        decompress("files/compressed_output.txt", "files/decompressed_output.txt");
+    }
+}
